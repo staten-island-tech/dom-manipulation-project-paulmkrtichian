b789ca5a67e6e75c21b9087d6f866308895ef09a
paulmkrtichian <paulmkr22@gmail.com>
2023-10-20 09:21:54 -0400

    10/20

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const DOMSelectors = {
     form: document.getElementById('userForm'),
     nameInput: document.getElementById('name'),
     favoritecolorInput: document.getElementById('favoritecolor'),
+    favoriteamimalInput: document.getElementById('favoriteanimal'),
     userDataList: document.getElementById('userData')
 };
 
-function createObject(name, favoritecolor) {
-    return { name, favoritecolor };
+function createObject(name, favoritecolor, favoriteamimal) {
+    return { name, favoritecolor, favoriteamimal};
 }
 
 function injectObject(obj) {
     const listItem = document.createElement('li');
-    listItem.textContent = `Name: ${obj.name}, Favorite Color: ${obj.favoritecolor}`;
+    listItem.textContent = `Name: ${obj.name}, Favorite Color: ${obj.favoritecolor}, Favorite Animal: ${obj.favoriteamimal}`;
     DOMSelectors.userDataList.appendChild(listItem);
 }
 
 function clearFields() {
     DOMSelectors.nameInput.value = '';
-    DOMSelectors.favoritecolorInput.value = '';
+    DOMSelectors.favoritecolorInput.value = ''
+    DOMSelectors.favoriteamimalInput.value;
 }
 
 function removeObject(item) {
@@ -30,8 +32,10 @@ DOMSelectors.form.addEventListener('submit', function(e) {
     e.preventDefault();
     const name = DOMSelectors.nameInput.value;
     const favoritecolor = DOMSelectors.favoritecolorInput.value;
+    const favoriteamimal = DOMSelectors.favoriteamimalInput.value
 
-    const userObject = createObject(name, favoritecolor);
+
+    const userObject = createObject(name, favoritecolor, favoriteamimal);
     injectObject(userObject);
     clearFields();
 });
@@ -49,3 +53,18 @@ function backgroundAndText(background, text){
 
 backgroundAndText(DOMSelectors.favoritecolorInput, DOMSelectors.nameInput)
 */
+function makeAlbum(){
+    //return an object
+}
+function addCard(Album) {
+DOMSelectors.form.addEventListener("submit", function (event){
+    event.preventDefault();
+}
+)};
+
+//afterbegin
+
+/*<div class="gallery"></div>
+<div class="card"></div class="card-title">Midnights</div><div>
+
+</div> (HTML) */
